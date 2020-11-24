6453e92faa2227c4f9a62231baa0b42d2346076a
jlynch25 <38656343+jlynch25@users.noreply.github.com>
2020-11-24 13:55:20 +0000

    change dependency release version

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,47 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import org.joda.time.DateTime;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+        // DateTime courseStart = new DateTime(2020, 01, 02, 20, 12);
+        // DateTime courseEnd = new DateTime(2020, 12, 30, 20, 12);
+        // Course course0 = new Course("CSIT", courseStart, courseEnd);
+
+        Module module0 = new Module("Software Enginering III", "CT417");
+
+        DateTime studentDob = new DateTime(2020, 01, 02, 20, 12);
+        Student student0 = new Student("Jack Lynch", 22, studentDob, 17370591,  "jlynch25");
+
+
+        // courseStart = new DateTime(2010, 02, 02, 20, 15);
+        // courseEnd = new DateTime(2020, 06, 30, 10, 6);
+        // Course course1 = new Course("CSIT", courseStart, courseEnd);
+
+        // Module module1 = new Module("Final Year Project", "CT413");
+
+        // studentDob = new DateTime(2000, 01, 02, 20, 12);
+        // Student student1 = new Student("nick sam", 22, studentDob, 25239316,  "nick");
+
+        // courseStart = new DateTime(1910, 02, 02, 20, 15);
+        // courseEnd = new DateTime(1920, 06, 30, 10, 6);
+        // Course course2 = new Course("BCS", courseStart, courseEnd);
+
+        // Module module2 = new Module("Machine Learning", "CT4101");
+
+        // studentDob = new DateTime(1998, 01, 02, 20, 12);
+        // Student student2 = new Student("steve man", 22, studentDob, 25239316,  "waz");
+
+        System.out.println( module0.getStudents() );
+
+        module0.addStudent(student0);
+
+
+        System.out.println( module0.getStudents() );
     }
 }
